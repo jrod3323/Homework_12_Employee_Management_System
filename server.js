295994276d8ee3956b3fc49275844c6d048ebe6e
Jared Ackley <jrod3323@gmail.com>
2021-01-09 17:27:38 -0600

    add lib folder containing const.js , prompts.js, queries.js, and added initial prompt and server connection

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
-const inqurier = require("inquirer");
-const mysql = require("mysql2")
+const mysql = require("mysql2");
+
+const {promptChoices} = require('./lib/prompts');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Bengals3323!",
+    database: "employeeManagement_DB"
+  });
+
+  connection.connect(async (err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    await runSearch();
+    connection.end();
+});
+
+async function runSearch() {
+
+    let answer, artist, range;
+
+    answer = await promptChoices();
+    console.table(answer.name);
+ 
+}
